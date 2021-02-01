ff9e7322ea97dfe0a75f9b907172c060641efad7
윤희수 <hsyoon@iparking.co.kr>
2021-02-01 11:07:30 +0900

    * Create

---
+++ pages/intro/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      INTRO
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'intro',
+  data() {
+    return {
+    }
+  },
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {},
+  computed: {}
+}
+</script>
+
+<style lang="scss">
+</style>
